5f4ba7e2d67a6513df104c96d2c47b13d63740d6
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2023-06-01 02:13:58 -0500

    Code V0.5

--- 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/ModifyAdministrator.java
+++ 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/ModifyAdministrator.java
@@ -8,7 +8,30 @@ import java.util.Scanner;
 /**
  *
  * @author Diego Casignia, Techware, DCCO-ESPE
+ * @author Gabriel Baez, Techware, DCCO-ESPE
  */
 public class ModifyAdministrator {
-
+    public void readAdministrator() throws FileNotFoundException{
+        EditAdministratorData editAdministratorData = new EditAdministratorData();
+        editAdministratorData.readAdministratorData();
+        
+    }
+    
+    public void createAdministrator(){
+        EditAdministratorData editAdministratorData = new EditAdministratorData();
+        Administrator administrator;
+        ArrayList<Administrator> administrator1 = new ArrayList<>();
+        Scanner readData = new Scanner(System.in);
+        System.out.print("Id: ");
+        int id = readData.nextInt();
+        System.out.print("Name: ");
+        String name = readData.next();
+        System.out.print("Age: ");
+        int age = readData.nextInt();
+        System.out.print("Phone Number: ");
+        String contactNumber = readData.next();
+        administrator = new Administrator(id, name, age,  contactNumber);
+        administrator1.add(administrator);
+        editAdministratorData.writeAdministratorData(administrator);
+}
 }
