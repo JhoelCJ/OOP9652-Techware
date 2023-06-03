d459aa70117442481752a0f6bcb24a631897fe49
Báez Jaramillo Gabriel Alejandro <132725537+GabrielBaezJ@users.noreply.github.com>
2023-06-03 12:19:57 -0500

    Code V0.8.2

--- 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/Sale.java
+++ 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/Sale.java
@@ -1,13 +1,29 @@
-package ec.edu.espe.panesrumiñahui.model;
+package ec.edu.espe.Sales.model;
 
+import java.util.Scanner;
 /**
  *
- * @author Diego Casignia, Techware, DCCO-ESPE
+ * @author Gabriel Baez, Techware, DCCO-ESPE
  */
-
 public class Sale {
-    //
-    public void sale(){
+    public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
+        double totalSales = 0;
+        int sale;
+        
+        System.out.print("Ingrese el número de ventas realizadas: ");
+        sale = scanner.nextInt();
+
+        for (int i = 1; i <= sale; i++) {
+            System.out.print("Ingrese el monto de la venta " + i + ": ");
+            double montoVenta = scanner.nextDouble();
+            totalSales += montoVenta;
+        }
+        
+        System.out.println("El total de ventas realizadas es: " + totalSales);
+       
+        scanner.close();
     }
 }
