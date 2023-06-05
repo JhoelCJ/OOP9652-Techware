76dc762692bf6d598abdbee51556ed4759131aae
Gcalvache <gabrielcalvache3@gmail.com>
2023-06-05 17:02:32 -0500

    CodeV0.9.1

--- 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/ModifyEmployee.java
+++ 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/ModifyEmployee.java
@@ -11,82 +11,152 @@ import java.util.Scanner;
  *
  * @author Diego Casignia, Techware, DCCO-ESPE
  */
-
 public class ModifyEmployee {
-    
-    public void readEmployee() throws FileNotFoundException{
+
+    public void readEmployee() throws FileNotFoundException {
         EditEmployeeData editEmployeeData = new EditEmployeeData();
         File archivo = new File("employee.json");
         ArrayList<Employee> listEmployees = new ArrayList<>();
         System.out.println("\t\t\t\tEmployed List");
-        
+
         if (archivo.exists() && archivo.length() == 0) {
             System.out.println("No hay empleados\n\n");
         } else {
             listEmployees = editEmployeeData.readEmployeeData();
-            for(Employee employee : listEmployees){
+            for (Employee employee : listEmployees) {
                 System.out.println(employee.toString());
             }
         }
     }
-    
-    public void createEmployee() throws FileNotFoundException{
+
+    public void createEmployee() throws FileNotFoundException {
         EditEmployeeData editEmployeeData = new EditEmployeeData();
         ArrayList<Employee> listEmployees = new ArrayList();
         Employee employee;
         Scanner readData = new Scanner(System.in);
-        System.out.print("\nId: ");
-        int id = readData.nextInt();
-        System.out.print("Nombre: ");
-        String name = readData.next();
-        System.out.print("Edad: ");
-        int age = readData.nextInt();
-        System.out.print("Anio que empezo atrabajar: ");
-        int year = readData.nextInt();
-        System.out.print("Mes que empezo atrabajar: ");
-        int month = readData.nextInt();
-        System.out.print("Dia que empezo atrabajar: ");
-        int day = readData.nextInt();
+        ValidationUtil validationUtil = new ValidationUtil();
+
+        // Validating id
+        int id = 0;
+        boolean validId = false;
+        do {
+            System.out.print("\nId: ");
+            String idInput = readData.next();
+            validId = validationUtil.isValidInt(idInput);
+            if (validId) {
+                id = Integer.parseInt(idInput);
+            } else {
+                System.out.println("El id debe ser un valor numerico.");
+            }
+        } while (!validId);
+
+        // Validating name
+        String name;
+        boolean validName = false;
+        do {
+            System.out.print("Nombre: ");
+            name = readData.next();
+            validName = validationUtil.isValidAlphaNumericString(name);
+            if (!validName) {
+                System.out.println("El nombre solo debe contener letras (mayusculas o minusculas)");
+            }
+        } while (!validName);
+
+// Validating age
+        int age = 0;
+        boolean validAge = false;
+        do {
+            System.out.print("Edad: ");
+            String ageInput = readData.next();
+            validAge = validationUtil.isValidInt(ageInput);
+            if (validAge) {
+                age = Integer.parseInt(ageInput);
+            } else {
+                System.out.println("La edad debe ser un valor numerico.");
+            }
+        } while (!validAge);
+
+// Validating year
+        int year = 0;
+        boolean validYear = false;
+        do {
+            System.out.print("Año que empezó a trabajar: ");
+            String yearInput = readData.next();
+            validYear = validationUtil.isValidInt(yearInput);
+            if (validYear) {
+                year = Integer.parseInt(yearInput);
+            } else {
+                System.out.println("El año debe ser un valor numérico.");
+            }
+        } while (!validYear);
+
+// Validating month
+        int month = 0;
+        boolean validMonth = false;
+        do {
+            System.out.print("Mes que empezó a trabajar: ");
+            String monthInput = readData.next();
+            validMonth = validationUtil.isValidInt(monthInput);
+            if (validMonth) {
+                month = Integer.parseInt(monthInput);
+            } else {
+                System.out.println("El mes debe ser un valor numérico.");
+            }
+        } while (!validMonth);
+
+// Validating day
+        int day = 0;
+        boolean validDay = false;
+        do {
+            System.out.print("Día que empezó a trabajar: ");
+            String dayInput = readData.next();
+            validDay = validationUtil.isValidInt(dayInput);
+            if (validDay) {
+                day = Integer.parseInt(dayInput);
+            } else {
+                System.out.println("El día debe ser un valor numérico.");
+            }
+        } while (!validDay);
+
+// Validating contact number
         String contactNumber;
         boolean validContactNumber = false;
         do {
-            System.out.print("Numero telefonico: ");
+            System.out.print("Número telefónico: ");
             contactNumber = readData.next();
-            String patronNumerico = "^[0-9]+$";
-            if (!contactNumber.matches(patronNumerico)) {
+            validContactNumber = validationUtil.isValidNumericString(contactNumber);
+            if (!validContactNumber) {
                 System.out.println("El número de contacto solo debe contener dígitos numéricos.");
-            } else {
-                validContactNumber = true;
             }
         } while (!validContactNumber);
-        
-        employee = new Employee(id, name, age, new Date(year-1900,month-1,day),  contactNumber);
-        
+
+        employee = new Employee(id, name, age, new Date(year - 1900, month - 1, day), contactNumber);
+
         File archivo = new File("employee.json");
         if (archivo.exists() && archivo.length() == 0) {
             listEmployees = new ArrayList<>();
         } else {
             listEmployees = editEmployeeData.readEmployeeData();
         }
-        
+
         listEmployees.add(employee);
         editEmployeeData.writeEmployeeData(listEmployees);
     }
-    
-    public void deleteEmployee() throws FileNotFoundException{
+
+    public void deleteEmployee() throws FileNotFoundException {
         EditEmployeeData editEmployeeData = new EditEmployeeData();
         File archivo = new File("employee.json");
         ArrayList<Employee> listEmployees = new ArrayList();
-        
+
         if (archivo.exists() && archivo.length() == 0) {
             System.out.println("No hay empleados\n\n");
         } else {
             listEmployees = editEmployeeData.readEmployeeData();
             Scanner readData = new Scanner(System.in);
-            
+
             System.out.print("Nombre del empleado a eliminar: ");
             String name = readData.next();
-            
+
             for (int i = 0; i < listEmployees.size(); i++) {
                 Employee employee = listEmployees.get(i);
                 if (employee.getName().equals(name)) {
---
+++ 06-Code/PanesRumiñahui/src/ec/edu/espe/panesRumiñahui/model/ValidationUtil.java
@@ -0,0 +1,27 @@
+package ec.edu.espe.panesrumiñahui.model;
+
+/**
+ * 
+ * 
+ * @author Calvache Gabriel, Techware, DCCO-ESPE
+ */
+public class ValidationUtil {
+
+    public boolean isValidInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean isValidNumericString(String input) {
+        return input.matches("^[0-9]+$");
+    }
+
+    public boolean isValidAlphaNumericString(String input) {
+        return input.matches("^[a-zA-Z]+$");
+    }
+}
+
